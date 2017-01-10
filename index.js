a141d091659c2eddf9a0037e85257fe7a0379587
joabakk <github@heiamoss.com>
2017-01-10 14:39:18 +0100

    my data settings in config

--- index.js
+++ index.js
@@ -18,6 +18,46 @@ module.exports = function(app) {
   plugin.schema = {
   type: "object",
   properties: {
+    refresh: {
+      type: "number",
+      title: "Refresh rate in seconds",
+      default: 300
+    },
+    vibrate: {
+      type: "number",
+      title: "Vibration (0 - Don't vibrate),(1 - Short vibrate), (2 - Double vibrate), (3 - Long vibrate)",
+      default: 0
+    },
+    font: {
+      type: "number",
+      title: "font size 1 - GOTHIC_14, 2 - GOTHIC_14_BOLD, 3 - GOTHIC_18, 4 - GOTHIC_18_BOLD, 5 - GOTHIC_24, 6 - GOTHIC_24_BOLD, 7 - GOTHIC_28, 8 - GOTHIC_28_BOLD",
+      default: 5
+    },
+    theme: {
+      type: "number",
+      title: "Theme, 0 for black and 1 for white",
+      default: 0
+    },
+    scroll: {
+      type: "number",
+      title: "Scroll content to offset (as percentage 0..100). If param not defined or >100 - position will be kept.",
+      default: 33
+    },
+    light: {
+      type: "number",
+      title: "0 - Do nothing, 1 - Turn pebble light on for short time",
+      default: 0
+    },
+    blink: {
+      type: "number",
+      title: "1..10 - Blink content count (blinks with black/white for \"count\" times)",
+      default: 0
+    },
+    updown: {
+      type: "number",
+      title: "0 use up/down buttons for scrolling, 1 use up/down buttons for update, appending up=1|2/down=1|2 params (1=short/2=long)",
+      default: 0
+    },
     elements: {
       type: "array",
       title: " ",
@@ -40,7 +80,7 @@ module.exports = function(app) {
             title: "Show as",
             type: "string",
             default: "",
-            "description": "show as",
+            "description": "Short name for small screen",
 
         }
       }
